cf2dac50675384b8c8c2e3bcf4d101c15bd00245
AmSmo <adam.smolenski@gmail.com>
2020-08-22 21:15:30 -0400

    walkthrough part 2

--- narf.rb
+++ narf.rb
@@ -7,7 +7,6 @@ class Episode
     def initialize(url)
         @url = HTTParty.get(url)
         @parsed_episode = parse_page
-        
     end
 
     def get_text(css)
@@ -19,7 +18,7 @@ class Episode
     end
 
     def title
-        get_text(parsed_episode.css('h1').map { |ele| ele.text.strip }[0])
+        get_text(parsed_episode.css('h1'))[0]
     end
 
     def airdate
@@ -28,5 +27,29 @@ class Episode
 
     def rating
         get_text(parsed_episode.css('[itemprop="ratingValue"]'))[0]
+    end
+
+    def plot
+        parsed_episode.at('.canwrap > p  > span').text.strip
+    end
+
+    def characters
+        chars = parsed_episode.css('td.character').map {|char| char.text.strip}
+        chars.map { |char| char.gsub(/[[:space:]]+/, " " ).strip}
+    end
+
+    def actors
+        parsed_episode.css('.primary_photo > a  > img').map {|inner| inner.attributes["title"].value}
+    end
+
+    def cast_assesment
+        i = 0
+        cast = {}
+        while i < actors.length
+            cast[actors[i]] = characters[i].split(" / ")
+            i+=1
+        end
+        
+        cast
     end
 end
--- run.rb
+++ run.rb
@@ -1,6 +1,5 @@
 require 'pry'
-require_relative './narf.rb'
+require_relative 'narf.rb'
 narf = Episode.new('https://www.imdb.com/title/tt0954793/?ref_=ttep_ep10')
 parsed_episode = narf.parsed_episode
 binding.pry
-"zort"
